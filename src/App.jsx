c0aa22a1e9a9f340b27d3d1b3f79423b10a2634d
Aislan <aislandnjunior@gmail.com>
2024-09-21 16:15:48 -0300

    🎉 initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './App.css'
+
+//Components
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './components/login/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
