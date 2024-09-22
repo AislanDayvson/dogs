5e80a623a91cc6454b98782dbab2581bbceacd84
Aislan <aislandnjunior@gmail.com>
2024-09-22 16:38:45 -0300

    ✅ added the hook useForm to validate fields in form

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import styles from './Input.module.css'
-const Input = ({label, type, name }) => {
+const Input = ({label, type, name, value, onChange , error, onBlur}) => {
   return (
     <div className={styles.wrapper}>
         <label htmlFor={name} className={styles.label}>{label}</label>
-         <input name={name} type={type} className={styles.input} id={name} />
-         <p className={styles.error}>Error</p>
+         <input 
+          name={name} 
+          type={type} 
+          className={styles.input} 
+          id={name} 
+          onChange={onChange} 
+          value={value} 
+          onBlur={onBlur} />
+         {error && (
+          <p className={styles.error}>{error}</p>
+         )}
+         
     </div>
    
   )
