bc2c78b79794e3d819504df0d543770df400bc89
Aislan <aislandnjunior@gmail.com>
2024-09-27 20:01:26 -0300

    ✅ added the user page and protectedroute

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/Home'
+import User from './Components/user/User'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/login/Login'
 import { UserStorage } from './userContext'
+import ProtectedRoute from './Components/helper/ProtectedRoute'
 
 const App = () => {
   return (
@@ -17,7 +19,8 @@ const App = () => {
           <Header />
             <Routes>
               <Route path='/' element={<Home />}/>
-              <Route path='/login/*' element={<Login />}/>
+              <Route path='login/*' element={<Login />}/>
+              <Route path='conta/*' element={<ProtectedRoute><User/></ProtectedRoute>}/>  
             </Routes>
           <Footer />
         </UserStorage>
