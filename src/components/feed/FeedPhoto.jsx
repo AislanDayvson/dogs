2ad75d9d225e004542ef49825616be2420eac750
Aislan <aislandnjunior@gmail.com>
2024-09-30 20:05:57 -0300

    ✅ added the feed to view photos

---
+++ src/components/feed/FeedPhoto.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import FeedPhotosItem from './FeedPhotosItem'
+import useFetch from '../../Hooks/useFetch'
+import { PHOTOS_GET } from '../../api'
+import Error from '../helper/Error'
+import Loading from '../helper/Loading'
+import styles from './FeedPhoto.module.css'
+
+const FeedPhoto = () => {
+    const {data, loading, error, request} = useFetch()
+
+    React.useEffect(() => {
+        const fetchPhotos = async () => {
+            const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0})
+            const {json} = await request(url, options)
+            console.log(json)
+        }
+        fetchPhotos()
+    }, [request])
+
+    if(error) return <Error error={error} />
+    if(loading) return <Loading />
+    if(data)
+  return (
+    <div>
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo}/>)}
+      </ul>
+    </div>
+  )
+  else return null
+}
+
+export default FeedPhoto
