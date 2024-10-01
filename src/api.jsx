0a41a4b7592e80f1c5d4ac04de73aaa81b0790c1
Aislan <aislandnjunior@gmail.com>
2024-10-01 11:57:16 -0300

    ✅ added the comments post

--- src/api.jsx
+++ src/api.jsx
@@ -83,5 +83,20 @@ export function USER_GET(token) {
     };
   }
 
+  export function COMMENT_POST(id, body) {
+    return {
+      url: `${API_URL}/api/comment/${id}`,
+      options: {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+        },
+        body: JSON.stringify(body),
+      },
+    };
+  }
+  
+
 
 
